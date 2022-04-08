d8f7aecb16440742b301a9079a3db608c11194be
Knarkzel <knarkzel@gmail.com>
2022-04-09 01:21:51 +0200

    flush

--- src/main.zig
+++ src/main.zig
@@ -40,22 +40,21 @@ export fn main(_: c_int, _: [*]const [*:0]const u8) noreturn {
     // GX boilerplate
     const buffer: [fifo_size]u32 = undefined;
     var fifo_buffer = c.MEM_K0_TO_K1(&buffer[0]) orelse unreachable;
+
     _ = c.GX_Init(fifo_buffer, fifo_size);
     // c.GX_SetCopyClear(c.GXColor{ .r = 0, .g = 0, .b = 0, .a = 255 }, 0x00ffffff);
-    c.GX_SetViewport(0, 0, @intToFloat(f32, rmode.fbWidth), @intToFloat(f32, rmode.efbHeight), 0, 1);
+    c.GX_SetViewport(0, 0, @intToFloat(f32, rmode.fbWidth), @intToFloat(f32, rmode.efbHeight), 0, 0);
     _ = c.GX_SetDispCopyYScale(@intToFloat(f32, rmode.xfbHeight) / @intToFloat(f32, rmode.efbHeight));
     c.GX_SetScissor(0, 0, rmode.fbWidth, rmode.efbHeight);
     c.GX_SetDispCopySrc(0, 0, rmode.fbWidth, rmode.efbHeight);
     c.GX_SetDispCopyDst(rmode.fbWidth, rmode.xfbHeight);
     c.GX_SetCopyFilter(rmode.aa, &rmode.sample_pattern, c.GX_TRUE, &rmode.vfilter);
-    if (rmode.viHeight == 2 * rmode.xfbHeight) {
-        c.GX_SetFieldMode(rmode.field_rendering, c.GX_ENABLE);
-    } else {
-        c.GX_SetFieldMode(rmode.field_rendering, c.GX_ENABLE);
-    }
+    c.GX_SetFieldMode(rmode.field_rendering, @boolToInt(rmode.viHeight == 2 * rmode.xfbHeight));
+
     c.GX_SetCullMode(c.GX_CULL_NONE);
     c.GX_CopyDisp(xfb, c.GX_TRUE);
     c.GX_SetDispCopyGamma(c.GX_GM_1_0);
+
     c.guPerspective(&projection, 60, 1.33, 10.0, 300.0);
     c.GX_LoadProjectionMtx(&projection, c.GX_PERSPECTIVE);
     c.GX_ClearVtxDesc();
@@ -72,7 +71,7 @@ export fn main(_: c_int, _: [*]const [*:0]const u8) noreturn {
 
     while (true) {
         c.guLookAt(&view, &camera, &up, &look);
-        c.GX_SetViewport(0, 0, @intToFloat(f32, rmode.fbWidth), @intToFloat(f32, rmode.efbHeight), 0, 1);
+        c.GX_SetViewport(0, 0, @intToFloat(f32, rmode.fbWidth), @intToFloat(f32, rmode.efbHeight), 0, 0);
         c.GX_InvVtxCache();
         c.GX_InvalidateTexAll();
         c.guMtxIdentity(&model);
@@ -91,7 +90,9 @@ export fn main(_: c_int, _: [*]const [*:0]const u8) noreturn {
 
         c.GX_DrawDone();
         c.GX_SetZMode(c.GX_TRUE, c.GX_LEQUAL, c.GX_TRUE);
+        c.GX_SetColorUpdate(c.GX_TRUE);
         c.GX_CopyDisp(xfb, c.GX_TRUE);
+        c.GX_Flush();
         c.VIDEO_WaitVSync();
     }
 }
