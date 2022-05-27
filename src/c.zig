c1903afc050b77e89b882bb729a0e97b28617063
Knarkzel <knarkzel@gmail.com>
2022-05-27 22:42:55 +0200

    It's building but not running

--- src/c.zig
+++ src/c.zig
@@ -1,4 +1,5 @@
 pub usingnamespace @cImport({
     @cInclude("gccore.h");
     @cInclude("stdio.h");
+    @cInclude("oggplayer.h");
 });
--- src/main.zig
+++ src/main.zig
@@ -18,6 +18,13 @@ export fn main(_: c_int, _: [*]const [*:0]const u8) noreturn {
     const video = Video.init();
     Console.init(video.mode, &stdout);
 
+    // Audio
+    const sample_ogg = &struct {
+        var bytes = @embedFile("sample.ogg").*;
+    }.bytes;
+    // const sample_ogg = &struct { bytes @embedFile("sample.ogg");
+    _ = c.PlayOgg(sample_ogg, sample_ogg.len, 5, c.OGG_ONE_TIME);
+
     while (true) {
         video.start();
         utils.triangle(.{ .{ 10, 10 }, .{ 210, 10 }, .{ 210, 210 } }, .{ 1, 1, 1 });
