482ce8f09fe23a07d6a214e3c1699c204118ae7f
StavenX <edvin_myhren@hotmail.com>
2022-06-07 01:41:45 +0200

    Draw blocks (no collision yet)

--- src/game/game.zig
+++ src/game/game.zig
@@ -12,6 +12,7 @@ const Sprite = enum {
     player_fall,
     player_sword,
     slime_idle,
+    block,
 
     fn draw(comptime self: Sprite, area: [4][2]f32) void {
         const settings: [4]f32 = switch (self) {
@@ -22,6 +23,7 @@ const Sprite = enum {
             .player_fall => .{ 32, 32, 32, 32 },
             .player_sword => .{ 64, 0, 32, 96 },
             .slime_idle => .{ 128, 0, 32, 32 },
+            .block => .{ 128, 96, 32, 32 },
         };
         // Current texture atlas size (textures.png)
         const size = .{ 256, 256 };
@@ -87,6 +89,22 @@ const Slime = struct {
     }
 };
 
+const Block = struct {
+    x: f32,
+    y: f32,
+    width: f32 = 32,
+    height: f32 = 32,
+
+    fn init(x: f32, y: f32) Block {
+        return .{ .x = x, .y = y };
+    }
+
+    fn drawSprite(self: *Block, comptime sprite: Sprite) void {
+        var area = utils.rectangle(self.x, self.y, self.width, self.height);
+        sprite.draw(area);
+    }
+};
+
 pub fn run(video: *Video) void {
     // Texture
     var texture = Gpu.init();
@@ -97,6 +115,12 @@ pub fn run(video: *Video) void {
 
     // Slime
     var slime = Slime.init(200, 200);
+
+    // Blocks
+    var blocks: [20]Block = undefined;
+    for (blocks) | *block, i | {
+        block.* = Block.init(0 + (@intToFloat(f32, i) * 32), 200);
+    } 
 
     while (true) {
         // Handle new players
@@ -190,6 +214,9 @@ pub fn run(video: *Video) void {
         if (slime.x + slime.width > 640) slime.direction = .left;
         if (slime.x < 0) slime.direction = .right;
 
+        // Block logic
+        for (blocks) | *block | block.drawSprite(.block);
+        
         video.finish();
     }
 }
