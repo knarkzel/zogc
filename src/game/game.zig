9759e43a3ff5d67c692a92ee1d538f777ff29f68
StavenX <edvin_myhren@hotmail.com>
2022-06-08 01:11:58 +0200

    Change sprite locations to include heart.png

--- src/game/game.zig
+++ src/game/game.zig
@@ -18,6 +18,7 @@ pub const Sprite = enum {
     player_sword,
     slime_idle,
     block,
+    heart,
 
     pub fn draw(comptime self: Sprite, area: [4][2]f32) void {
         const settings: [4]f32 = switch (self) {
@@ -28,7 +29,8 @@ pub const Sprite = enum {
             .player_fall => .{ 32, 32, 32, 32 },
             .player_sword => .{ 64, 0, 32, 96 },
             .slime_idle => .{ 128, 0, 32, 32 },
-            .block => .{ 128, 96, 32, 32 },
+            .heart => .{ 128, 96, 32, 32 },
+            .block => .{ 160, 96, 32, 32 },
         };
         utils.sprite(area, settings, .{ 256, 256 });
     }
