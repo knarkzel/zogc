dcb731a92d24036b457241b4c373fb46d2c48748
Knarkzel <knarkzel@gmail.com>
2022-06-10 05:18:15 +0200

    Major build.zig refactor

--- build.zig
+++ build.zig
@@ -1,10 +1,13 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const Builder = std.build.Builder;
+const allocPrint = std.fmt.allocPrint;
 
 const name = "zogc";
 const wii_ip = "192.168.11.171";
-const devkitpro = "vendor/devkitpro";
+const textures = "src/game/textures";
+const packages = "vendor";
+const devkitpro = packages ++ "/devkitpro";
 const dolphin = switch (builtin.target.os.tag) {
     .macos => "Dolphin",
     .windows => "Dolphin.exe",
@@ -12,15 +15,6 @@ const dolphin = switch (builtin.target.os.tag) {
 };
 
 pub fn build(b: *Builder) !void {
-    // ensure depencies in vendor are installed
-    const vendor = "vendor";
-    const dir = try std.fs.cwd().openDir(vendor, .{ .iterate = true });
-    var iter = dir.iterate();
-    while (try iter.next()) |entry| {
-        const module = try std.fmt.allocPrint(b.allocator, "{s}/{s}", .{ vendor, entry.name });
-        try package(b.allocator, module);
-    }
-
     // set build options
     const mode = b.standardReleaseOptions();
     const obj = b.addObject("main", "src/main.zig");
@@ -38,6 +32,34 @@ pub fn build(b: *Builder) !void {
         .cpu_model = .{ .explicit = &std.Target.powerpc.cpu.@"750" },
         .cpu_features_add = std.Target.powerpc.featureSet(&.{.hard_float}),
     });
+
+    // ensure dependencies are installed
+    {
+        const dir = try std.fs.cwd().openDir(packages, .{ .iterate = true });
+        var iter = dir.iterate();
+        while (try iter.next()) |entry| {
+            const module = try allocPrint(b.allocator, "{s}/{s}", .{ packages, entry.name });
+            try package(b.allocator, module);
+        }
+    }
+
+    // ensure images in textures are converted to tpl
+    {
+        const dir = try std.fs.cwd().openDir(textures, .{ .iterate = true });
+        var iter = dir.iterate();
+        while (try iter.next()) |entry| {
+            if (std.mem.endsWith(u8, entry.name, ".png")) {
+                const base = entry.name[0 .. entry.name.len - 4];
+                const input = try allocPrint(b.allocator, "{s}/{s}", .{ textures, entry.name });
+                const output = try allocPrint(b.allocator, "{s}/{s}.tpl", .{ textures, base });
+                try convert(b.allocator, input, output);
+
+                // Delete useless extra header file
+                const header = try allocPrint(b.allocator, "{s}/{s}.h", .{ textures, base });
+                try std.fs.cwd().deleteFile(header);
+            }
+        }
+    }
 
     // build both elf and dol
     const elf = b.addSystemCommand(&.{ devkitpro ++ "/devkitPPC/bin/powerpc-eabi-gcc", "build/main.o", "-g", "-DGEKKO", "-mrvl", "-mcpu=750", "-meabi", "-mhard-float", "-Wl,-Map,build/.map", "-L" ++ devkitpro ++ "/libogc/lib/wii", "-lmad", "-lasnd", "-logc", "-lm", "-o", "build/" ++ name ++ ".elf" });
@@ -68,22 +90,22 @@ pub fn build(b: *Builder) !void {
             line_step.dependOn(&addr2line.step);
         }
     }
-
-    // const conv_step = b.step("tpl", "Converts image into tpl");
-    // if (b.args) |args| {
-    // for (args) |arg| {
-    // const path = b.pathJoin(&.{ cwd(), "/", arg });
-    // const gxtexconv = b.addSystemCommand(&.{ devkitpro ++ "/tools/bin/gxtexconv", "-i", path });
-    // conv_step.dependOn(&gxtexconv.step);
-    // }
-    // }
 }
 
-// Ensures package is installed with git submodule
-fn package(allocator: std.mem.Allocator, path: []const u8) !void {
-    var child = std.ChildProcess.init(&.{ "git", "submodule", "update", "--init", path }, allocator);
+fn command(allocator: std.mem.Allocator, argv: []const []const u8) !void {
+    var child = std.ChildProcess.init(argv, allocator);
     child.cwd = std.fs.path.dirname(@src().file) orelse ".";
     child.stderr = std.io.getStdErr();
     child.stdout = std.io.getStdOut();
     _ = try child.spawnAndWait();
+}
+
+// Ensures package is installed with git submodule
+fn package(allocator: std.mem.Allocator, path: []const u8) !void {
+    try command(allocator, &.{ "git", "submodule", "update", "--init", "--recursive", path });
+}
+
+// Converts image into tpl format
+fn convert(allocator: std.mem.Allocator, input: []const u8, output: []const u8) !void {
+    try command(allocator, &.{ devkitpro ++ "/tools/bin/gxtexconv", "-i", input, "-o", output });
 }
